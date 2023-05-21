7ef1c8e95a88775ad9e6a5af1877f543c88fdf87
nights192 <nights192@gmail.com>
2023-05-21 18:03:29 -0500

    Assemble framework for mesh construction.

---
+++ Scripts/MeshBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISubmeshBuilder
+{
+    public (int[], Material) BuildSubmesh();
+}
+
+public class RetainedSubmesh : ISubmeshBuilder
+{
+    public int[] Triangles;
+    public Material Material;
+
+    public RetainedSubmesh(int[] triangles, Material material)
+    {
+        Triangles = triangles;
+        Material = material;
+    }
+
+    public (int[], Material) BuildSubmesh()
+    {
+        return (Triangles, Material);
+    }
+}
+
+public class MeshBuilder
+{
+    Mesh mesh;
+    MeshRenderer renderer;
+
+    List<int[]> tris;
+    List<Material> materials;
+
+    public MeshBuilder(Mesh mesh, MeshRenderer renderer) {
+        this.mesh = mesh;
+        this.renderer = renderer;
+    }
+
+    public void Add(ISubmeshBuilder submeshBuilder)
+    {
+        (int[] submeshTris, Material materialTris) = submeshBuilder.BuildSubmesh();
+
+        tris.Add(submeshTris);
+        materials.Add(materialTris);
+    }
+}
